f85602bd29b16fa6d06e3764d77de6010b63520a
zevi bem <zevibem@gmail.com>
2023-08-27 14:03:46 +0300

    ## 5. Shape with Draw Method

--- index.ts
+++ index.ts
@@ -29,60 +29,60 @@
 // }
 
 // 3. Shape - צורה
-class Shape {
-    constructor() {}
-    info(): string {
-        return "This is a Shape"
-    }
-}
+// class Shape {
+//     constructor() {}
+//     info(): string {
+//         return "This is a Shape"
+//     }
+// }
 
-class Rectangle extends Shape{
-    width: number;
-    hight: number;
+// class Rectangle extends Shape{
+//     width: number;
+//     hight: number;
 
-    constructor(width: number, hight: number) {
-        super()
-        this.width = width;
-        this.hight = hight;
-    }
-    area(): number {
-        return (this.width * this.hight) / 2
-    }
+//     constructor(width: number, hight: number) {
+//         super()
+//         this.width = width;
+//         this.hight = hight;
+//     }
+//     area(): number {
+//         return (this.width * this.hight) / 2
+//     }
     
-    info(): string {
-        return ("This is a Rectangle")
-    }
-    scale(x:number): this{
-        this.width *= x 
-        this.hight *= x 
-        return this
-    }
+//     info(): string {
+//         return ("This is a Rectangle")
+//     }
+//     scale(x:number): this{
+//         this.width *= x 
+//         this.hight *= x 
+//         return this
+//     }
 
-    static double(rectangle1: Rectangle, rectangle2: Rectangle): Rectangle {
-       const newRectangle = new Rectangle (
-        rectangle1.width + rectangle2.width,
-        rectangle1.hight + rectangle2.width
-       )
-       return newRectangle
-    }
-}
-const  rectangle11 = new Rectangle(4, 6);
-const  rectangle12 = new Rectangle(5, 7);
+//     static double(rectangle1: Rectangle, rectangle2: Rectangle): Rectangle {
+//        const newRectangle = new Rectangle (
+//         rectangle1.width + rectangle2.width,
+//         rectangle1.hight + rectangle2.width
+//        )
+//        return newRectangle
+//     }
+// }
+// const  rectangle11 = new Rectangle(4, 6);
+// const  rectangle12 = new Rectangle(5, 7);
 
-console.log(Rectangle.double(rectangle11,rectangle12))
+// console.log(Rectangle.double(rectangle11,rectangle12))
 
-class ColoredRectangle extends Rectangle{
-    color: string;
+// class ColoredRectangle extends Rectangle{
+//     color: string;
 
-    constructor(width: number, hight: number, color: string) {
-        super(width, hight,)
-        this.color = color
-    }
-    info(): string {
-        return (`this rectangle with the color ${this.color}`)
-    }
+//     constructor(width: number, hight: number, color: string) {
+//         super(width, hight,)
+//         this.color = color
+//     }
+//     info(): string {
+//         return (`this rectangle with the color ${this.color}`)
+//     }
 
-}
+// }
 
 
 
@@ -90,9 +90,36 @@ class ColoredRectangle extends Rectangle{
 // 4. Method Chaining
 
 // 5. Shape with draw method
-// class Shape {
-//     draw() {
-//         console.log("drawing a shape")
-//     }
-// }
+class Shape {
+    draw(): void {
+        console.log("drawing a shape")
+    }
+}
+class Triangle extends Shape{
+    draw(): void {
+        console.log("drawing a Triangle")
+    }
+}
+class Circle extends Shape {
+    draw(): void {
+        console.log("drawing a Circle")
+    }
+}
+class Square extends Shape {
+    draw(): void {
+        console.log("drawing a Square")
+    }
+}
+
+function renderShapes(arr: Shape[]):void {
+    arr.forEach((element) => {
+        element.draw()
+    })
+}
+const circle = new Circle()
+const triangle = new Triangle()
+const square = new Square()
+
+const myArr = [circle, triangle, square]
+renderShapes(myArr)
 
